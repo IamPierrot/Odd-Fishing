095f8a9fc7a81aed10b4bbb2c6e977caa2fcc9ed
IamPierrot <phatnguyentan836@gmail.com>
2024-04-21 17:54:56 +0700

    add: test environment

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Test {
+import com.neyuq.oddfishing.OddFishing;
+import com.neyuq.oddfishing.Utils.ConfigurationManager;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class Test {
+    public static void main(String[] args) {
+
+        new OddFishing();
+
+    }
 }
