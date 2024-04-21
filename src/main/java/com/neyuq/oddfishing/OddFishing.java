a36dc43cb1a03f4a9c31e5d0ccc1765c1e48125b
IamPierrot <phatnguyentan836@gmail.com>
2024-04-21 17:51:36 +0700

    feat: add root file plugin and root resources

--- src/main/java/com/neyuq/oddfishing/OddFishing.java
+++ src/main/java/com/neyuq/oddfishing/OddFishing.java
@@ -1,17 +1,52 @@
 package com.neyuq.oddfishing;
 
+import com.neyuq.oddfishing.Commands.CommandFish;
+import com.neyuq.oddfishing.Database.CollectionManager;
+import com.neyuq.oddfishing.Listeners.FishingListener;
+import com.neyuq.oddfishing.Utils.ConfigurationManager;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public final class OddFishing extends JavaPlugin {
+
+    private Logger console;
+    private ConfigurationManager configurationManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        console = getServer().getLogger();
 
+        // Load Configuration
+        configurationManager = ConfigurationManager.getInstance(this);
+        configurationManager.loadConfig("fish-event.yml");
+
+        // Add listener and events
+        LoadListenerAndEvents();
+        // load Command
+        RegisterCommand();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        CollectionManager.disconnect();
+        configurationManager.saveConfigs();
+
+        console.log(Level.OFF,"§cPlugin đã sao lưu config và ngắt kết nối với database!");
+    }
+
+
+    private void LoadListenerAndEvents() {
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        pluginManager.registerEvents(new FishingListener(console), this);
+    }
+
+    private void RegisterCommand() {
+        Objects.requireNonNull(getCommand("fish")).setExecutor(new CommandFish());
     }
 }
